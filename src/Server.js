8d83bf3378139eb971ea87437aabcd595dc2b9d2
muhammad amin <muhammadamin137340@gmail.com>
2026-07-23 13:36:39 +0500

    feta

--- src/Server.js
+++ src/Server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB=require("./Config/db")
+const userRoutes = require("./routes/userRoutes");
 const PORT = process.env.PORT;
+app.use(express.json());
 
 const app = express();
 dotenv.config();
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Server running");
-});
+
+app.use("/api/users", userRoutes);
 
 app.listen( PORT,() => {
   console.log('http://localhost:5000');
